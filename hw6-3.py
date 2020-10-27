ad79ef94003d55ad465a6ea7b9782b1b1320ffab
Kyle <p21kbautista@fairfieldprep.org>
2020-10-27 13:29:47 -0400

    Add hw6-3.py

---
+++ hw6-3.py
@@ -0,0 +1,18 @@
+# Kyle Bautista (AMDG) 10/27/20
+
+import math
+
+# Question 1
+print(math.sqrt(529))
+
+# Question 2
+print(math.sin(0))
+
+#Q uestion 3
+print(math.cos(2 * math.pi))
+
+# Question 4
+print(math.pow(2.5, 5))
+
+# Question 5
+print(math.log(e [,base]))
